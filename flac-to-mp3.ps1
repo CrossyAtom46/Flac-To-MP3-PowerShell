1325e5e3e4b39256ddbfcf4155558fb4f3525844
CrossyAtom46 <82151617+CrossyAtom46@users.noreply.github.com>
2023-08-03 13:21:47 +0300

    Update flac-to-mp3.ps1

--- flac-to-mp3.ps1
+++ flac-to-mp3.ps1
@@ -1,7 +1,7 @@
 ﻿# Path to the input folder containing the files to convert
 $sourceFolder = "C:\Music"
 
-# Path to the output folder to save the converted OGG files
+# Path to the output folder to save the converted MP3 files
 $destinationFolder = "C:*Converted"
 
 # Path to the FFmpeg executable
@@ -9,7 +9,7 @@ $ffmpegPath = "C:\ffmpeg\ffmpeg.exe"
 # Get all FLAC files in the source folder
 $files = Get-ChildItem -Path $sourceFolder -Filter *.flac -File
 
-# Iterate through each file and convert it to OGG using fdkaac
+# Iterate through each file and convert it to MP3 using fdkaac
 foreach ($file in $files) {
     # Build the output file path with the same name and OGG extension
     $outputFilePath = Join-Path -Path $destinationFolder -ChildPath ($file.BaseName + ".mp3")
